70dcbf53e86fdbd6fb5a7b0c2e6bdb41a7fceb2e
Clifton Gockul <clifga@gmail.com>
2023-03-07 17:15:26 -0500

    completed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+return input.addEventListener('click', clickAlert); 
 }
+
+const input = document.getElementById('button');
+function clickAlert() {
+    alert('I was clicked!');
+  }
